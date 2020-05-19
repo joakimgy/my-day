a975f4035fd4179335b79a12835c41aeabc6e6ca
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-05-19 22:06:25 +0200

    Move all weather components to one place

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { auth, User } from "firebase";
 import { useUser } from "reactfire";
 import { motion } from "framer-motion";
-import Rain from "./Weather/Rain";
-import Sun from "./Weather/Sun";
 import CitySearch from "./CitySearch";
 import Weather from "./Weather/Weather";
 import useWeather from "../hooks/useWeather";
@@ -14,46 +12,42 @@ const ProfilePage = () => {
   const weather = useWeather(city);
 
   return (
-    <Rain>
-      <motion.div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        initial={{ scale: 0.7 }}
-        animate={{ scale: 1.0, borderRadius: "10%" }}
-      >
-        <h2>What's up {user.displayName}?</h2>
-        <div style={{ minHeight: "20vh" }}>
-          <Sun />
+    <motion.div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      initial={{ scale: 0.7 }}
+      animate={{ scale: 1.0, borderRadius: "10%" }}
+    >
+      <h2>What's up {user.displayName}?</h2>
+
+      {!city && (
+        <div>
+          <p>Select a city</p>
+          <CitySearch onSelect={setCity} />
         </div>
-        {!city && (
-          <div>
-            <p>Select a city</p>
-            <CitySearch onSelect={setCity} />
-          </div>
-        )}
-        {city && (
-          <div>
-            <p>You have selected {city}</p>
-            {weather && <Weather weatherCondition={weather.main} />}
-          </div>
-        )}
-        <div style={{ minHeight: "20vh" }} />
-        <motion.button
-          className="logout"
-          onClick={() => {
-            auth().signOut();
-          }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          Sign out
-        </motion.button>
-      </motion.div>
-    </Rain>
+      )}
+      {city && (
+        <div>
+          <p>You have selected {city}</p>
+          {weather && <Weather weatherCondition={weather.main} />}
+        </div>
+      )}
+      <div style={{ minHeight: "20vh" }} />
+      <motion.button
+        className="logout"
+        onClick={() => {
+          auth().signOut();
+        }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        Sign out
+      </motion.button>
+    </motion.div>
   );
 };
 export default ProfilePage;
--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { WeatherCondition } from "../../api/weather";
+import Sun from "./Sun";
+import Rain from "./Rain";
 
 type WeatherProps = {
   weatherCondition: WeatherCondition;
 };
 
 function Weather({ weatherCondition }: WeatherProps) {
-  return <p>It looks like the weather is {weatherCondition}</p>;
+  return (
+    <>
+      <Sun />
+      <Rain />
+      <p>It looks like the weather is {weatherCondition}</p>
+    </>
+  );
 }
 
 export default Weather;
