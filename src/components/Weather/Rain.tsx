0c80d98758bc8385163cb5a75fc62adfb15a867f
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-05-19 23:42:57 +0200

    Use CSS-IN-JS and framer motion for rain animation

--- src/components/Weather/Rain.tsx
+++ src/components/Weather/Rain.tsx
@@ -1,19 +1,35 @@
-import React, { ReactNode } from "react";
-import "./Rain.scss";
+import React, { ReactNode, CSSProperties } from "react";
+import { motion } from "framer-motion";
+import { random } from "lodash";
 
-type RainProps = {
-  children?: ReactNode;
-};
-
-function Rain({ children }: RainProps) {
+function Rain() {
   return (
     <>
-      <div>
-        {Array.from(Array(150)).map((_, index) => (
-          <div key={index} className="rain" />
-        ))}
-      </div>
-      {children}
+      {Array.from(Array(150)).map((_, index) => {
+        const horizontalPosition = random(100);
+        const verticalPosition = -(100 + random(50));
+        const rain: CSSProperties = {
+          background: "linear-gradient(to bottom, blue 0%, #ffffff 100%)",
+          height: "100%",
+          position: "absolute",
+          width: "3px",
+          zIndex: -1,
+          left: `${horizontalPosition}%`,
+        };
+
+        return (
+          <motion.div
+            key={index}
+            animate={{
+              opacity: [(random(30) + 30) * 0.01, 0],
+              top: [`${verticalPosition}%`, "0%"],
+              animationDelay: `${random(20) - 1}s`,
+            }}
+            transition={{ duration: random(6) + 4, loop: Infinity }}
+            style={rain}
+          />
+        );
+      })}
     </>
   );
 }
