e0240bd06d5a04f4a5c80c2f708a1af295501878
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-06-04 02:13:28 +0200

    Remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from "react";
-import Application from "./components/Application";
-import { FirebaseAppProvider } from "reactfire";
-import { firebaseConfig } from "./firebase";
+import React, { useState } from "react";
 import ErrorBoundary from "./components/Error/ErrorBoundary";
+import CitySearch, { City } from "./components/CitySearch";
+import useWeather from "./hooks/useWeather";
+import { motion } from "framer-motion";
+import CityOverview from "./components/CityOverview";
 
 export function App() {
+  const [city, setCity] = useState<City>();
+  const weather = useWeather(city);
+
   return (
     <ErrorBoundary>
-      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-        <Application />
-      </FirebaseAppProvider>
+      <motion.div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        initial={{ scale: 0.5 }}
+        animate={{ scale: 1.0, borderRadius: "10%" }}
+      >
+        <CitySearch setCity={setCity} />
+        <CityOverview city={city} weather={weather} />
+        <div style={{ minHeight: "20vh" }} />
+      </motion.div>
     </ErrorBoundary>
   );
 }
