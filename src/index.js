cebd4589f05c695a6d7960d19df9194be17edbe7
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-06-21 17:26:54 +0200

    Downgrade from experimental to latest React

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 
-ReactDOM.unstable_createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
